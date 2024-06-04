fd1ba287f28b820fe3f34a6f8478619f9613bab2
VidhiKhati <167620348+VidhiKhati@users.noreply.github.com>
2024-06-04 05:37:33 +0000

    conversion

--- basics/conversion.js
+++ basics/conversion.js
@@ -21,3 +21,37 @@ console.log(booleanisloggedin)
 let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof someNumber)
+
+// operations
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1"+ 2) //12
+console.log( 1 +"2") //12
+console.log( 1 + 2 + "2") //32
+console.log("1" + 2 + 2) //122
+console.log((3+4)*5%3)
+
+console.log(+true) //1
+console.log(+""); //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 3+3
+
+let a=100;
+a++;
+console.log(a) //101
