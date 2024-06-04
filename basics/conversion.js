8b2a8a8bea753b3afa283c6173e2be3952e6f01c
VidhiKhati <167620348+VidhiKhati@users.noreply.github.com>
2024-06-04 05:20:34 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,23 @@
+let score=undefined
+
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+//NaN means not a number
+//"33" = 33
+//"33abc" = NaN
+// true = 1 false=0
+//"Vidhi" = NaN
+
+let isLoggedIn = " "
+let booleanisloggedin = Boolean(isLoggedIn)
+console.log(booleanisloggedin)
+
+// 1= true 0=false
+//""= false
+//"hello" = true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof someNumber)
